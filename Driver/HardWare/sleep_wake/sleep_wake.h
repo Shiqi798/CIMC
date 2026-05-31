614d017d772dfea362e3d1cdc55023f42c09ba50
Shi-qi1717 <17shi17qi17@gmail.com>
2026-05-31 19:19:38 +0800

    低功耗，ad3344，报文初步 低功耗加了standby，lightsleep（uasrt唤醒），deepsleep ad3344读取正常，接口封装的比较全面 写了报文，但是没有很好用，而且发出来是hex模式

--- Driver/HardWare/sleep_wake/sleep_wake.h
+++ Driver/HardWare/sleep_wake/sleep_wake.h
@@ -5,7 +5,10 @@
 #define RTC_MAGIC      0x7050
 #include "HeaderFiles.h"
 
+void PA0_wakeup_init(void);
 void RTC_SetWakeup(uint32_t sec);
+void UART_LightSleep_Enter(void);
+void Standby_Enter(uint32_t sec);
 
 
 
